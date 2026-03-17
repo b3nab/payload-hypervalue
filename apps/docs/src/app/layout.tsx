151f5f0c47f7ac81be32874bd12606fc77138e92
Ben <b3nab@users.noreply.github.com>
2026-03-17 17:59:47 +0100

    docs: update layout

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next'
 import './global.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -10,7 +11,16 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>{children}
+          <Script
+            src={`${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/script.js`}
+            data-site-id={process.env.NEXT_PUBLIC_RYBBIT_ID}
+            data-web-vitals="true"
+            data-track-errors="true"
+            data-session-replay="true"
+            strategy="afterInteractive"
+          />
+        </RootProvider>
       </body>
     </html>
   )
