19007a65ccac397bf62104dee6c2796fe2867420
Ben <b3nab@users.noreply.github.com>
2026-03-20 17:54:03 +0100

    chore: example hypervalue local API

--- packages/payload-hypervalue/dev/app/my-route/route.ts
+++ packages/payload-hypervalue/dev/app/my-route/route.ts
@@ -6,7 +6,27 @@ export const GET = async (request: Request) => {
     config: configPromise,
   })
 
+  const book = await payload.find({
+    collection: "books",
+    limit: 1
+  })
+
+  if(!book.docs[0]) return
+  const bookID = book.docs[0].id
+
+  const counter = await payload.hypervalue.count({
+    //   ^?
+    collection: "books",
+    field: "price"
+  })
+  const {docs, totalDocs} = await payload.hypervalue.history({
+    //     ^?
+    collection: "books",
+    id: bookID
+  })
+
   return Response.json({
     message: 'This is an example of a custom route.',
   })
 }
+44
